406e24108866e726c0091b0da49eae8238f52b81
rosalesnikho <rosales.nikho@gmail.com>
2019-06-20 09:41:33 -0500

    changed dao method order

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -40,8 +40,8 @@ public class MySQLUsersDao implements Users {
             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, user.getUsername());
             stmt.setString(2, user.getEmail());
-            stmt.setString(3, user.getPreferences());
-            stmt.setString(4, user.getPassword());
+            stmt.setString(3, user.getPassword());
+            stmt.setString(4, user.getPreferences());
 
 
             stmt.executeUpdate();
